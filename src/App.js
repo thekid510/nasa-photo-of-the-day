3ef1bc03e3c99d0e11bba76e6662a8dee3e065c2
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-14 13:14:42 -0700

    Initial comnmit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <nav>
+       Nasa Photo of the Day <span role="img" aria-label='go!'>🚀</span>!
+      </nav>
     </div>
   );
 }
