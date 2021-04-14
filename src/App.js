7ae6ffbbb0e0be4872b79dca8683feba245f7d0b
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-14 14:12:52 -0700

    Fetched data with axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [nasaData, setNasaData] = useState([])
+  useEffect(()=>{
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=")
+    .then(res=>{
+      setNasaData(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  },[])
   return (
     <div className="App">
       <nav>
