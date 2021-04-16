38035a6ce205b03375ba17cd52d97b316d30d1a2
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-15 21:45:07 -0700

    added styled component button with date of image

--- src/Components/Nasa.js
+++ src/Components/Nasa.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import styled from 'styled-components'
+const StyledButton = styled.button`
+height: 100px,
+display: "flex",
+border: "1px solid black",
+padding: 8px;,
+justifyContent: "center",
+  alignItems: "center",
+  flex-direction:column
+}
 
+`
 const Data = (props) => {
 const { nasaData } = props;
+
+
 
 return (
     <div>
         { nasaData && <img src ={nasaData.url} alt="nasa"/>}
             {console.log(nasaData)}
+            <StyledButton className = "date">
+                {nasaData.date}
+            </StyledButton>
     </div>
 )
 }
